047f5b1bbfae7f3db0af34f9f78a387bbfc6146b
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-05-14 10:27:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var studentData = [
+    { name: "RAZIA",
+        age: 18,
+        address: "Hno 155",
+        rollno: 101,
+        city: "Karachi",
+        country: "Pakistan" },
+    { name: "ISHA",
+        age: 17,
+        address: "Hno E1",
+        rollno: 102,
+        city: "Karachi",
+        country: "Pakistan" },
+    { name: "Ayesha",
+        age: 25,
+        address: "456 Street",
+        rollno: 103,
+        city: "Karachi",
+        country: "Pakistan" },
+    { name: "Warda",
+        age: 26,
+        address: "Street 45",
+        rollno: 104,
+        city: "Karachi",
+        country: "Pakistan" }
+];
+console.log(studentData[0], studentData[3]);
